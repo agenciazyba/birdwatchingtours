e14a253e0413d6567445458bf4ab7c40da4f434c
Rico Pessoal <RicoPessoal@MacBook-Pro-de-Ricardo.local>
2025-01-21 21:18:24 -0500

    Ajuste server.js

--- server.js
+++ server.js
@@ -60,42 +60,6 @@ app.post('/api/submit-form', async (req, res) => {
     }
 });
 
-
-// Rota para receber os dados do formulário PLANYOURTRIP
-app.post("/PlanYourTrip", async (req, res) => {
-    const { fullName, email, targetBird, preferredMonth, groupSize } = req.body;
-
-    try {
-        const response = await fetch("https://api.notion.com/v1/pages", {
-            method: "POST",
-            headers: {
-                "Authorization": `Bearer ${process.env.NOTION_API_KEY}`,
-                "Content-Type": "application/json",
-                "Notion-Version": "2022-06-28",
-            },
-            body: JSON.stringify({
-                parent: { database_id: process.env.NOTION_DATABASE_PLANTRIP_ID},
-                properties: {
-                    FullName: { title: [{ text: { content: fullName } }] },
-                    Email: { email: email },
-                    Birds: { rich_text: [{ text: { content: targetBird } }] },
-                    Month: { rich_text: [{ text: { content: preferredMonth } }] },
-                    GroupSize: { number: groupSize },
-                },
-            }),
-        });
-
-        if (response.ok) {
-            res.status(200).json({ message: "Formulário enviado com sucesso!" });
-        } else {
-            const error = await response.json();
-            res.status(400).json({ message: "Erro ao enviar os dados.", details: error });
-        }
-    } catch (error) {
-        res.status(500).json({ message: "Erro interno no servidor.", error: error.message });
-    }
-});
-
 // Inicia o servidor
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
